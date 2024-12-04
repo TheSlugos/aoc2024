6522f78a7454674b1d31ba88054238691f234c5d
Stephen Powell <Stephen.Powell@healthcareaustralia.com.au>
2024-12-04 10:35:20 +1030

    day 3 one regex

---
+++ day03/day03_oneregex.py
@@ -0,0 +1,32 @@
+import re
+
+def process_mul(mul_text):
+    patt = "\\d+"
+    mat = list(map(int, re.findall(patt, mul_text)))
+    
+    return mat[0] * mat[1]
+        
+
+with open("input.txt") as f:
+    sum1 = 0
+    sum2 = 0
+    active = True
+    switch_patt = "mul\\(\\d{1,3},\\d{1,3}\\)|do\\(\\)|don't\\(\\)"
+
+    for line in f:
+        # find active ranges
+        switch_matches = re.finditer(switch_patt, line)
+        for s in switch_matches:
+            if active and s.group() == "don't()":
+                active = False
+            elif not(active) and s.group() == "do()":
+                active = True
+            elif "mul" in s.group():
+                # should be mul as the only other option
+                prod = process_mul(s.group())
+                sum1 = sum1 + prod
+                if active:
+                    sum2 = sum2 + prod
+    
+    print(sum1)
+    print(sum2)
